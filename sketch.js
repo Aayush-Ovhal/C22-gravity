3c7bd21b200ecfd811143af2f9f3bb17f9782bfb
Aayush-Ovhal <61664556+Aayush-Ovhal@users.noreply.github.com>
2020-03-21 10:07:38 +0530

    Updated correct code files

--- sketch.js
+++ sketch.js
@@ -1,48 +1,90 @@
+
+// CREATE GLOBAL VARIABLES
+// For Engine, World, Bodies and any other that you have in mind to make your coding life easier.
+// remember to create an array of boxes.
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine, world;
-var ground,ball;
+var engine,world,ground,block;
 
-function setup(){
-    var canvas = createCanvas(400,400);
+var gSlider;
+var RectH,RectW;
+ 
+ 
+function setup() {
+    createCanvas(400, 400);
+
+    // Create an instance of Engine, World
     engine = Engine.create();
     world = engine.world;
+
+    gSlider = createSlider(0, 100, 50);
+    gSlider.position(40, 365);
+    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
 
     var ground_options ={
         isStatic: true
     }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
-
-    var  ball_options ={
+    var block_options ={
         restitution: 1.0
     }
-    ball = Bodies.circle(200,100,50,ball_options);
-    
 
-    World.add(world,ball);
+    ground = Bodies.rectangle(200,400,400,50,ground_options);
+    RectH=50;
+    RectW=50;
+    block = Bodies.rectangle(200,100,RectW,RectH,block_options);
 
-    console.log(ground);
-}
+    World.add(world,ground);
+    World.add(world,block);
 
-function mousePressed() {
-   
-
-        // Every time a mouse press occures create a new box.
+   console.log(block.position.y)
  
-        
-    
 }
-function draw(){
-    background(0);
+ 
+
+ 
+function draw() {
+    // Draw all the elements including the slider that 
+    background(225);
     Engine.update(engine);
+    fill("grey");
     rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
-    ellipseMode(RADIUS);
-    fill("red");
-    ellipse(ball.position.x,ball.position.y,50);   
-    
+    rect(ground.position.x,ground.position.y,400,50);
+    fill("black");
+    rect(block.position.x,block.position.y,RectW,RectH);
+    //drawRect(block.position.x,block.position.y,30,50);
+    // This is the value of your gravity. You can optionally show it to the viewer.
+    var fVal = gSlider.value();
+    text("Gravity " + fVal, 160, 50);
+ 
+    // Use a for loop to show all the boxes
+
+   // drawSprites();
 }
+
+function mouseClicked() {
+    createCanvas(400, 400);
+    engine = Engine.create();
+    world = engine.world;
+    RectW = random(10,50);
+    RectH = random(10,50);
+
+    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
+
+    var ground_options ={
+        isStatic: true
+    }
+    ground = Bodies.rectangle(200,400,400,50,ground_options);
+    World.add(world,ground);
+
+    var block_options ={
+        restitution: 1.0
+    }
+    block = Bodies.rectangle(200,100,RectW,RectH,block_options);
+    World.add(world,block);
+ }
+ 
+
